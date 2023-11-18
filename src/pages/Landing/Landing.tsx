a4a055a75f13fa5e58ed9b1859c049526406f381
Juan <juandiegovelsol@gmail.com>
2023-11-18 10:59:08 -0500

    feat: initial layout configured

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,17 @@
+/* import { useState } from "react"; */
+import "./landing.scss";
+
+const Landing = () => {
+  return (
+    <main className="landing">
+      <section className="landing__container">
+        <h1 className="landing__title">Welcome to Wizybot!</h1>
+        <p className="landing__subtitle">
+          Select which type of information do you want to add to our system
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default Landing;
